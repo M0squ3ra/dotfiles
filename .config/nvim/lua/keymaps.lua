e2746f6f4d6c1cb215a995cb630f471bf736eee2
Carlos <carlosmosquera1998@gmail.com>
2024-05-16 20:55:44 -0300

    added shortcut for telescope live_grep

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,5 +1,6 @@
 vim.keymap.set('n', 'nt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', 'ff', ':Telescope find_files hidden=true<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'fg', ':Telescope live_grep<CR>', { noremap = true, silent = true})
 vim.keymap.set('n', '<F8>', ':TagbarToggle<CR>', { noremap = true, silent = true })
 -- Mapeo de teclas para pasar al siguiente buffer
 vim.keymap.set('n', '<C-l>', ':bn<CR>', { noremap = true, silent = true })
